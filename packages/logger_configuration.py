30fa86b3c53da894403b4ce4f8faf7810384a158
Szymon Kluziak (Nokia) <szymon.kluziak@nokia.com>
2024-04-07 14:10:55 +0200

    add logging file removal

--- packages/logger_configuration.py
+++ packages/logger_configuration.py
@@ -1,7 +1,12 @@
 import logging
-
+import pathlib
 
 def configure_logger():
+    log_file = pathlib.Path("debug.log")
+
+    if log_file.is_file():
+        log_file.unlink()
+
     main_logger = logging.getLogger(__name__.split('.')[0])
     main_logger.setLevel(logging.DEBUG)
 
@@ -13,7 +18,7 @@ def configure_logger():
     main_logger.addHandler(console_handler)
 
 
-    debug_handler = logging.FileHandler(filename="debug.log")
+    debug_handler = logging.FileHandler(filename=log_file)
     debug_handler.setLevel(logging.DEBUG)
     DEBUG_LOG_FORMAT = '[%(asctime)-15s] [%(levelname)-8s] [%(name)-50s] [%(module)-15s] %(message)s (%(funcName)s:%(lineno)d)'
     debug_formatter = logging.Formatter(DEBUG_LOG_FORMAT)
