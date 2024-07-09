a9d5072be71eb12a3f0daa17120bc3b82f59c626
Szymon Kluziak (Nokia) <szymon.kluziak@nokia.com>
2024-07-09 15:11:12 +0200

    update hand-eye calibration

--- packages2/handeye.py
+++ packages2/handeye.py
@@ -78,30 +78,6 @@ class HandEyeCalibration:
             for i in range(3):
                 matrix[i, 3] = pos[i]
             matrix[3, 3] = 1
-            self._logger.info(f'Obtained matrix: \n{matrix}')
+            self._logger.info(f'Obtained matrix: \n\t\t{matrix}')
             robot_datas.append(matrix)
         return chess_datas, robot_datas
-
-
-
-
-
-# if __name__ == "__main__":
-#     HOST = "192.168.0.1"
-#     PORT = 10000
-#     print("Start listening...")
-#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#     s.bind((HOST, PORT))
-#     s.listen(5)
-#     c, addr = s.accept()
-
-    
-#     configure_logger("debug_handeye")
-#     handeye = HandEyeCalibration(c)
-#     a, positions  = handeye.run()
-#     print(f"{len(a)}")
-#     pos_list = [list(pos) for pos in positions]
-#     print(pos_list)
-#     handeye.prepare_data(a, pos_list)
-#     print("Done!") 
