366449d332ddef4ba993151e7c785875319278e6
Szymon Kluziak (Nokia) <szymon.kluziak@nokia.com>
2024-07-10 16:59:24 +0200

    initial add Tsai&Li calibration algoritms

--- packages2/handeye.py
+++ packages2/handeye.py
@@ -1,12 +1,10 @@
 from packages2.robot import Robot
 from packages2.camera import Camera
 import cv2 
-# import socket
 import numpy as np
-from packages2.common import RPY_to_rmtx
+import packages2.common as common
 import logging
 from packages2.image_processor import ImageProcessor
-
 
 class HandEyeCalibration:
     def __init__(self, robot_client):
@@ -101,7 +99,7 @@ class HandEyeCalibration:
             roll = pos[3]
             pitch = pos[4]
             yaw = pos[5]
-            rotation_matrix = RPY_to_rmtx(roll, pitch, yaw)
+            rotation_matrix = common.RPY_to_rmtx(roll, pitch, yaw)
             matrix[:3, :3] = rotation_matrix
             for i in range(3):
                 matrix[i, 3] = pos[i]
@@ -112,14 +110,83 @@ class HandEyeCalibration:
 
 
     def calibrate_tsai(self):
-        
+        A_list = self.camera_mtx.copy()
+        B_list = self.robot_mtx.copy()
 
-        pass
+        N = len(A_list)
+        S = None
+        RA_I = None
+        T = None
+        TA = None
+        TB = None
+
+        for i in range(N):
+            An = A_list[i]
+            Bn = B_list[i]
+            RA = An[:3,:3]
+            tA = An[:3,3].reshape(3,1)
+            tA_ = common.skew2(tA)
+            RB = Bn[:3,:3]
+            tB = Bn[:3,3].reshape(3,1)
+            
+            uA, wA = common.R_2_angle_axis(RA)
+            uB, wB = common.R_2_angle_axis(RB)
+            _S = common.skew2(uA + uB)
+            _T = uB - uA 
+            _RA_I = RA - np.eye(3)
+            _TA = tA
+            _TB = tB
+            
+            S = np.append(S, _S, axis=0) if S is not None else _S
+            T = np.append(T, _T, axis=0) if T is not None else _T
+            
+            RA_I = np.append(RA_I,_RA_I,axis=0) if RA_I is not None else _RA_I
+            TA = np.append(TA,_TA,axis=0) if TA is not None else _TA
+            TB = np.append(TB,_TB,axis=0) if TB is not None else _TB
+
+        ux = common.solveLS(S,T)    
+        uX = 2*ux/(np.sqrt(1+common.norm(ux)**2))
+        Rx = (1-common.norm(uX)**2/2)*np.eye(3) + 0.5*(uX*uX.T + np.sqrt(4-common.norm(uX)**2)*common.skew2(uX))
+        tX = common.get_Translation(Rx,RA_I,TA,TB)
+        print(f"Calculated Tsai results | Rx: {Rx}")
+        print(f"Calculated Tsai results | tX: {tX.reshape(3,1)}")
+        return Rx, tX.reshape(3,1)
 
 
     def calibrate_li(self):
+        A_list = self.camera_mtx.copy()
+        B_list = self.robot_mtx.copy()
+        
+        N = len(A_list)
+        I = np.eye(3)
+        I9 = np.eye(9)
+        S = None
+        T = None
 
+        for i in range(N):
+            An = A_list[i]
+            Bn = B_list[i]
+            RA = An[:3,:3]
+            tA = An[:3,3].reshape(3,1)
+            tA_ = common.skew2(tA)
+            RB = Bn[:3,:3]
+            tB = Bn[:3,3].reshape(3,1)
+            S1 = np.append(I9 - np.kron(RB, RA), np.zeros((9,3)), axis=1)
+            S2 = np.append(np.kron(tB.T, tA_), np.dot(tA_, (I - RA)), axis=1)
 
-        pass
+            _S = np.append(S1, S2, axis=0)
+            _T = np.append(np.zeros((9,1)), tA.reshape(3,1), axis=0)
+            
+            S = np.append(S, _S, axis=0) if S is not None else _S
+            T = np.append(T, _T, axis=0) if T is not None else _T
+
+        Rx_tX = common.solveSVD(S)
+        Rx, tX = common.get_RX_tX(Rx_tX)
+        Rx = common.getRotation(Rx)
+
+        print(f"Calculated Li results | Rx: {Rx}")
+        print(f"Calculated Li results | tX: {tX.reshape(3,1)}")
+
+        return Rx, tX.reshape(3,1)
 
 
