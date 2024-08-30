cb568dc5a2dbcb67b77569127edd8ea06ae02628
Szymon Kluziak (Nokia) <szymon.kluziak@nokia.com>
2024-08-30 12:53:14 +0200

    little cleanup in camera calibration

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ MAIN PROGRAM TO CONTROL UR COBOT WITH VISUAL SYSTEM
 from packages2.start_communication import start_communication
 from packages2.robot_functions import RobotFunctions
 from packages2.camera_calibrator import CameraCalibrator
+from packages2.handeye_eye_in_hand_NEW import HandEyeCalibration
 
 
 
@@ -20,14 +21,18 @@ c, s = start_communication()
 
 # initialization
 robot_functions = RobotFunctions(c)
-camera_calibrator = CameraCalibrator(c)
+
 
 # camera calibration
+camera_calibrator = CameraCalibrator(c)
 camera_calib_result_path = camera_calibrator.run()
 print(f"camera_calib_result_path: {camera_calib_result_path}")
 
 
 
+# hand-eye calibration
+# handeye_calibrator = HandEyeCalibration(camera_calib_result_path, c)
+# handeye_calibrator.run()
 
 # end communication
 c.close()
--- packages2/camera_calibrator.py
+++ packages2/camera_calibrator.py
@@ -1,13 +1,12 @@
 import time
-from packages2.cmd_generator import CmdGenerator  # Ensure this module is available
-from packages2.robot_positions import RobotPositions  # Ensure this module is available
-from packages2.robot_functions import RobotFunctions  # Ensure this module is available
-from packages2.image_processor import ImageProcessor  # Ensure this module is available
+from packages2.cmd_generator import CmdGenerator
+from packages2.robot_positions import RobotPositions
+from packages2.robot_functions import RobotFunctions
+from packages2.image_processor import ImageProcessor
 import socket
 import cv2
 import threading
 import numpy as np
-import pickle
 import packages2.common as common
 import datetime
 from pathlib import Path
@@ -18,7 +17,6 @@ from scipy.spatial.transform import Rotation as R
 class CameraCalibrator:
     def __init__(self, c=None):
         self.c = c
-
         self.max_iter = 30
         self.min_accuracy = 0.001
         self.chess_size = (8,7)
@@ -32,11 +30,11 @@ class CameraCalibrator:
             ret, frame = cap.read()
             if not ret:
                 break
-            cv2.imshow("All Frames", frame)  # Display each frame
+            cv2.imshow("All Frames", frame)
             if frame_event.is_set():
-                frame_storage['frame'] = frame  # Store frame in dictionary on request
-                frame_event.clear()  # Reset event after storing frame
-            if cv2.waitKey(1) == ord('q'):  # Allow exiting the loop by pressing 'q'
+                frame_storage['frame'] = frame
+                frame_event.clear()
+            if cv2.waitKey(1) == ord('q'):
                 break
 
         cap.release()
@@ -47,37 +45,38 @@ class CameraCalibrator:
         stop_event = threading.Event()
         frame_storage = {}
         robot_poses = RobotPositions()
-        # Create a new thread for the camera function
+
         camera_thread = threading.Thread(target=self.show_camera, args=(frame_event, frame_storage, stop_event))
         camera_thread.start()
 
-        # HOST = "192.168.0.1"
-        # PORT = 10000
-        # print("Start listening...")
-        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        # s.bind((HOST, PORT))
-        # s.listen(5)
-        # c, addr = s.accept()
         robot_functions = RobotFunctions(self.c)
 
         try:
-            msg = self.c.recv(1024)
-            print(msg)
-            if msg == b"Hello server, robot here":
-                print("Robot requested for data!")
-                time.sleep(0.5)
+            robot_functions.moveJ(RobotPositions.look_at_chessboard)
 
-                robot_functions.moveJ(RobotPositions.look_at_chessboard)
+            folder_with_time = "images_" + datetime.datetime.now().strftime("%d-%m_%H-%M")
+            waiting_pos_folder = folder_with_time + "/waiting_pos"
+            directory_with_time = Path("data/results/for_camera_calib/"+folder_with_time)
+            directory_with_time.mkdir(parents=True, exist_ok=True)
 
-                folder_with_time = "images_" + datetime.datetime.now().strftime("%d-%m_%H-%M")
-                waiting_pos_folder = folder_with_time + "/waiting_pos"
-                directory_with_time = Path("data/results/for_camera_calib/"+folder_with_time)
-                directory_with_time.mkdir(parents=True, exist_ok=True)
+            directory_waiting = Path("data/results/for_camera_calib/"+waiting_pos_folder)
+            directory_waiting.mkdir(parents=True, exist_ok=True)
+            i = 0
 
-                directory_waiting = Path("data/results/for_camera_calib/"+waiting_pos_folder)
-                directory_waiting.mkdir(parents=True, exist_ok=True)
-                i = 0
+            frame_event.set()  # Signal camera thread to capture frame
+            
+            # Wait for the frame to be stored
+            while frame_event.is_set():
+                time.sleep(0.1)  # Wait a bit before checking again
+
+            # Now the frame should be available in frame_storage['frame']
+            if 'frame' in frame_storage:
+                _img = frame_storage['frame']
+                img_name = f"{directory_waiting}/WAITING_POSE.jpg"
+                cv2.imwrite(img_name, _img)
+
+            for pose in robot_poses.camera_calib_poses:
+                robot_functions.moveJ(pose)
 
                 frame_event.set()  # Signal camera thread to capture frame
                 
@@ -87,51 +86,35 @@ class CameraCalibrator:
 
                 # Now the frame should be available in frame_storage['frame']
                 if 'frame' in frame_storage:
-                    _img = frame_storage['frame']
-                    img_name = f"{directory_waiting}/WAITING_POSE.jpg"
-                    cv2.imwrite(img_name, _img)
+                    i+=1
+                    test_img = frame_storage['frame']
+                        
+                    gray = test_img.copy()
+                    gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
+                    ret, corners = cv2.findChessboardCorners(gray, (8,7), None)
+                    if not ret:
+                        print("****** PHOTO SKIPPED ******")
+                        continue
+                    cv2.imshow("Requested Frame", test_img)
 
-                for pose in robot_poses.camera_calib_poses:
-                    robot_functions.moveJ(pose)
+                    img_name = f"{directory_with_time}/img_{"{:03d}".format(i)}.jpg"
+                    cv2.imwrite(img_name, test_img)
+                    print(f"Saved {img_name}")
+                    cv2.waitKey(500)
+                    cv2.destroyWindow("Requested Frame")
+                else:
+                    print("Frame was not captured")
+                time.sleep(1)
 
-                    frame_event.set()  # Signal camera thread to capture frame
-                    
-                    # Wait for the frame to be stored
-                    while frame_event.is_set():
-                        time.sleep(0.1)  # Wait a bit before checking again
 
-                    # Now the frame should be available in frame_storage['frame']
-                    if 'frame' in frame_storage:
-                        i+=1
-                        test_img = frame_storage['frame']
-                            
-                        gray = test_img.copy()
-                        gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
-                        ret, corners = cv2.findChessboardCorners(gray, (8,7), None)
-                        if not ret:
-                            print("****** PHOTO SKIPPED ******")
-                            continue
-                        cv2.imshow("Requested Frame", test_img)
-
-                        img_name = f"{directory_with_time}/img_{"{:03d}".format(i)}.jpg"
-                        cv2.imwrite(img_name, test_img)
-                        print(f"Saved {img_name}")
-                        cv2.waitKey(500)  # Wait for any key press
-                        cv2.destroyWindow("Requested Frame")
-                    else:
-                        print("Frame was not captured")
-                    time.sleep(1)
-
-                print("DONE")
-                # cv2.destroyAllWindows()
-                return True, directory_with_time
 
         except socket.error as socketerror:
             print("Socket error: ", socketerror)
         finally:
             stop_event.set()  # Signal the camera thread to stop
             camera_thread.join()  # Wait for the camera thread to finish
-
+            print("DONE")
+            return True, directory_with_time
 
 
 
@@ -212,8 +195,6 @@ class CameraCalibrator:
         print('Camera Calibrated!')
         print(f'Calculated camera matrix: {self.camera_matrix}')
         print(f"Calculated distortion parameters: {self.distortion_params}")
-        # print(f'Calculated rotation vectors: {rvecs}')
-        # print(f'Calculated translation vectors: {tvecs}')
 
         # calculate reprojection error
         mean_error = 0 
@@ -230,4 +211,4 @@ class CameraCalibrator:
         if ret:
             print("ret is true")
             self.run_calibration(flow_files_path)
-        return flow_files_path
+        return f"{flow_files_path}/CameraParams.npz"
