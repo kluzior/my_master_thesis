837736b8b1fe7730344f2554a2cb2c1a5e9caeca
Szymon Kluziak (Nokia) <szymon.kluziak@nokia.com>
2024-09-02 10:15:53 +0200

    add variables to collect or not new photos to main

--- main.py
+++ main.py
@@ -11,17 +11,15 @@ from packages2.handeye_eye_in_hand_NEW import HandEyeCalibration
 
 from packages2.logger_configuration import configure_logger
 
+COLLECT_CAMERA_CALIB_DATA = False
+COLLECT_HAND_EYE_CALIB_DATA = False
+
+
+
 c = None
-# CAMERA CALIBRATION FILES (uncomment when you want to not collect new data)
-cc_files = "data/results/for_camera_calib/images_30-08_15-25"
-
-# HAND_EYE CALIBRATION FILES (uncomment when you want to not collect new data)
-he_files = "data/results/for_hand_eye_calib/images_30-08_15-26"   # wszystkie pomiary
-
-
 
 # start communication
-# c, s = start_communication()
+c, s = start_communication()
 
 # initialization
 robot_functions = RobotFunctions(c)
@@ -29,16 +27,21 @@ configure_logger()
 
 # camera calibration
 camera_calibrator = CameraCalibrator(c)
-# camera_intrinsic_path = camera_calibrator.run()
-camera_intrinsic_path = camera_calibrator.run(cc_files)
+if COLLECT_CAMERA_CALIB_DATA:
+    camera_intrinsic_path = camera_calibrator.run()
+else:
+    cc_files = "data/results/for_camera_calib/images_30-08_15-25"
+    camera_intrinsic_path = camera_calibrator.run(cc_files)
 print(f"camera_intrinsic_path: {camera_intrinsic_path}")
-
 
 
 # hand-eye calibration
 handeye_calibrator = HandEyeCalibration(camera_intrinsic_path, c)
-# handeye_calibrator.run()
-handeye_calibrator.run(he_files)
+if COLLECT_HAND_EYE_CALIB_DATA:
+    handeye_calibrator.run()
+else:
+    he_files = "data/results/for_hand_eye_calib/images_02-09_09-56"
+    handeye_calibrator.run(he_files)
 
 # test run to few points on chess board
 handeye_calibrator.send_robot_to_test_poses(he_files, handeye_type='tsai')
