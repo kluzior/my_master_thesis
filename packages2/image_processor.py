ebe76c26602bf3d0bd60d7c20f81b47068f098ad
Szymon Kluziak <sz.kluziak@gmail.com>
2024-09-05 16:50:30 +0200

    delete unused functions

--- packages2/image_processor.py
+++ packages2/image_processor.py
@@ -115,22 +115,6 @@ class ImageProcessor:
             _, rvecs, tvecs = cv2.solvePnP(objp, corners, mtx, None)    # None dist_param because image is already undistorted!
         return rvecs, tvecs, corners[idx]
 
-    def show_chess_corner(self, frame, chess_size=(8,7), point=(0,0)):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        ret, corners2 = cv2.findChessboardCorners(gray, chess_size, None)
-        if ret:
-            corners_mtx = np.array(corners2).reshape(chess_size[1], chess_size[0], 2)
-            
-            image_to_show = frame.copy()
-            
-            center = corners_mtx[point[1], point[0]]
-            center = tuple(center.astype(int))
-
-            cv2.circle(image_to_show, center, radius=10, color=(0, 0, 255), thickness=5)
-            cv2.imshow('POINT GUIDANCE', image_to_show)
-        else:
-            self._logger.error("Nie znaleziono narożników szachownicy.")
-
     def get_roi_points(self, frame):                
         print("Please select ROI point on picture!")
 
@@ -183,33 +167,3 @@ class ImageProcessor:
         cy = int(M["m01"] / M["m00"])
         return np.array([cx, cy], dtype=np.float32)
     
-
-    # NOT TESTED
-    def is_centroid_applicable_for_gripper(self, centroid, contours, frame, scale_factor=1):
-        """
-        Check if the centroid is applicable for the gripper.
-        
-        :param centroid: Tuple of (x, y) coordinates for the centroid.
-        :param contours: List of contours to check against.
-        :param frame: The image frame to draw on and check.
-        :param scale_factor: Scale factor to convert mm to pixels.
-        :return: Boolean indicating if the centroid is applicable.
-        """
-        gripper_radius_mm = 20
-        gripper_radius_pixels = gripper_radius_mm * scale_factor
-
-        mask = np.zeros_like(frame, np.uint8)
-
-        cv2.circle(mask, centroid, int(gripper_radius_pixels), (255, 255, 255), -1)
-
-        for contour in contours:
-            # Create a mask for the current contour
-            contour_mask = np.zeros_like(self.image, np.uint8)
-            cv2.drawContours(contour_mask, [contour], -1, (255, 255, 255), -1)
-
-            # Check for intersection
-            intersection = cv2.bitwise_and(mask, contour_mask)
-            if np.any(intersection > 0):
-                return False
-
-        return True
