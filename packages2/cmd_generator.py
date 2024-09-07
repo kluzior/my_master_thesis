eec5d14aac57267a67344afd1c3683e7bba67bdc
Szymon Kluziak <sz.kluziak@gmail.com>
2024-09-07 14:25:43 +0200

    cleanup in cmd_generator

--- packages2/cmd_generator.py
+++ packages2/cmd_generator.py
@@ -1,5 +1,4 @@
 class CmdGenerator:
-
     def basic(message):
         return message.encode('utf-8')
     
@@ -21,22 +20,4 @@ class CmdGenerator:
                 f"{pose.get('Ry')}," +\
                 f"{pose.get('Rz')}" +\
                 f")" + "\n"
-        return cmd.encode('utf-8')
-
-    def movej_gen(pose, acc=0.5, vel=0.5):
-        cmd = f"movej([{pose.get('base')}," +\
-                f"{pose.get('shoulder')}," +\
-                f"{pose.get('elbow')}," +\
-                f"{pose.get('wrist1')}," +\
-                f"{pose.get('wrist2')}," +\
-                f"{pose.get('wrist3')}" +\
-                f"], a = {acc}, v = {vel})" + "\n"
-        return cmd.encode('utf-8')
-
-    def set_output(out_no):
-        cmd = f"set_digital_out({out_no}, True)" + "\n"
-        return cmd.encode('utf-8')
-
-    def reset_output(out_no):
-        cmd = f"set_digital_out({out_no}, False)" + "\n"
         return cmd.encode('utf-8')
