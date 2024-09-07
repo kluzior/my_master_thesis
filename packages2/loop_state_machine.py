11038132fc38c598a313c06c3799a84aa0af7c47
Szymon Kluziak <sz.kluziak@gmail.com>
2024-09-07 14:56:55 +0200

    cleanup in loop state

--- packages2/loop_state_machine.py
+++ packages2/loop_state_machine.py
@@ -1,23 +1,18 @@
-from packages2.robot_functions import RobotFunctions
-from packages2.robot_positions import RobotPositions
 from packages2.image_processor import ImageProcessor
-from packages2.plane_determination import PlaneDeterminator
 from packages2.handeye_eye_in_hand import HandEyeCalibration
-from packages2.common import show_camera, pose2rvec_tvec, pose_list_to_dict, joints_list_to_dict
 from packages2.neural_network import NN_Classificator
-
+from packages2.plane_determination import PlaneDeterminator
+from packages2.robot_functions import RobotFunctions
+from packages2.robot_poses import RobotPoses
+from packages2.common import show_camera, pose2rvec_tvec, calculate_center_point
 import cv2
 import threading
 import time
 import numpy as np
 import logging
-
-from packages2.robot_poses import RobotPoses
 import random
-from pathlib import Path
 import datetime
-
-
+from pathlib import Path
 
 class LoopStateMachine:
     def __init__(self, c, camera_intrinsic_path, handeye_path, mlp_model_path, timestamp):
@@ -40,7 +35,7 @@ class LoopStateMachine:
         self.timestamp = timestamp
 
         self._logger = logging.getLogger(f'{__name__}.{self.__class__.__name__}')
-        self._logger.debug(f'ImageProcessor({self}) was initialized.')
+        self._logger.debug(f'LoopStateMachine({self}) was initialized.')
 
         folder_with_time = "images_" + timestamp
         self.directory_with_time = Path("data/results/identification/"+folder_with_time)
@@ -76,7 +71,6 @@ class LoopStateMachine:
 
     def state_initialization(self):
         self._logger.info("Initializing robot...")
-
         self.state = 'GoToWaitPosition_init'
 
     def state_go_to_wait_position_init(self):
@@ -90,7 +84,6 @@ class LoopStateMachine:
             _img = self.frame_storage['frame']
             uimg, _ = self.ip.undistort_frame(_img, self.camera_mtx, self.dist_params)
             self.table_roi_points = self.ip.get_roi_points(uimg)
-
         if self.table_roi_points is not None:
             self.state = 'GoToWaitPosition'
 
@@ -102,7 +95,6 @@ class LoopStateMachine:
             self.state = 'IdentificationContours'
 
     def state_identification_contours(self):
-        contours_identified = False
         self._logger.info("Identifying contours...")
         self.frame_event.set()  # signal camera thread to capture frame
         while self.frame_event.is_set():
@@ -113,64 +105,23 @@ class LoopStateMachine:
             gray = cv2.cvtColor(uimg, cv2.COLOR_BGR2GRAY)
             buimg = self.ip.apply_binarization(gray)
             final_img, _ = self.ip.ignore_background(buimg, self.table_roi_points)
-            
-            cv2.imshow("prepared picure", final_img)
-            cv2.waitKey(1000)
-            cv2.destroyWindow("prepared picure")   
-
             contours, _ = cv2.findContours(final_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             contours = [cnt for cnt in contours if cv2.contourArea(cnt) > 600]
-
-            img_to_show_elipse2 = uimg.copy()
-
             for contour in contours:
-                target_pixel = self.calculate_center_point(contour)
+                target_pixel, angle = calculate_center_point(contour)
                 cropped_image, contour_frame = self.ip.crop_contour(final_img, contour)
                 contour_prediction, contour_probability = self.nnc.predict_shape(cropped_image, image_size=(64,64))
-                # self.add_record2object(contour_prediction, contour_probability, target_pixel, contour_frame)
-
-                # Obliczanie momentów konturu
-                M = cv2.moments(contour)
-
-                if M['m00'] != 0:
-                    # Środek masy konturu
-                    cx = int(M['m10'] / M['m00'])
-                    cy = int(M['m01'] / M['m00'])
-
-                    # Obliczanie kąta orientacji
-                    angle = 0.5 * np.arctan2(2 * M['mu11'], (M['mu20'] - M['mu02']))
-
-                    # Rysowanie środka masy
-                    cv2.circle(img_to_show_elipse2, (cx, cy), 5, (0, 0, 255), -1)
-
-                    # Rysowanie osi konturu
-                    length = 100  # Długość osi
-                    x2 = int(cx + length * np.cos(angle))
-                    y2 = int(cy + length * np.sin(angle))
-                    cv2.line(img_to_show_elipse2, (cx, cy), (x2, y2), (255, 0, 0), 2)
-                    cv2.putText(img_to_show_elipse2, str(angle), (cx, cy + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, [0, 0, 255], 2)
-
-
                 self.add_record2object(contour_prediction, contour_probability, target_pixel, contour_frame, angle)
 
-
-        cv2.imshow("img_to_show_elipse2", img_to_show_elipse2)
-
-        ### VISUALISE IDENTIFICATION RESULTS
+        ### VISUALISE & SAVE IDENTIFICATION RESULTS
         img_identification_result = self.nnc.visualize(uimg, self.objects_record)
         cv2.imshow("identification results", img_identification_result)
-
-        ### ADD SAVING THIS IMAGE TO FILES!!!
         _img_path = f"{self.directory_with_time}/{datetime.datetime.now().strftime("%H-%M-%S")}.jpg"
         _uimg_path = f"{self.directory_with_time}/uimg_{datetime.datetime.now().strftime("%H-%M-%S")}.jpg"
         cv2.imwrite(_img_path, img_identification_result)
         cv2.imwrite(_uimg_path, uimg)
-        self.iter_count += 1
-        print(f"ITER COUNT: {self.iter_count}")
-
         cv2.waitKey(3000)
         cv2.destroyWindow("identification results")
-        cv2.destroyWindow("img_to_show_elipse2")
 
         if not self.objects_record:
             self.state = 'WaitForObjects'
@@ -203,34 +154,21 @@ class LoopStateMachine:
         pose = self.pd.pixel_to_camera_plane(target_pixel)
         robot_pose = self.rf.give_pose()
         rotation_matrix, _, tvec = pose2rvec_tvec(list(robot_pose))
-
         target_pose, _ = self.he.calculate_point_pose2robot_base_new(self.pd.rmtx, pose.reshape(-1, 1), rotation_matrix, tvec, self.handeye_path)
         ret = self.rf.pick_object(target_pose, angle)
-
         if ret == 0:
             self.state = 'GoToWaitPosition'
+            # self.state = 'PickUpAndMove'
 
     def state_pick_up_and_move(self, label):
         self._logger.info("Picking up object and moving to position...")
         self.rf.moveJ_pose(self.robposes.before_banks)
-        object_height = 0.008
         bank_pose = self.robposes.banks[label].copy()
-        bank_pose["z"] += (self.bank_counters[label] + 1) * object_height
-
+        bank_pose["z"] += (self.bank_counters[label] + 1) * self.objects_height
         ret = self.rf.drop_object(bank_pose)
-
         self.bank_counters[label] += 1
-
         if ret == 0:
-            self.state = 'ReturnToWaitPosition'
-
-    def state_return_to_wait_position(self):
-        self._logger.info("Returning to wait position...")
-
-        wait_position_reached = True
-        if wait_position_reached:
             self.state = 'GoToWaitPosition'
-
 
     def add_record2object(self, label, prediction, pixel_coords, contour_frame, angle):
         record = {
@@ -267,10 +205,3 @@ class LoopStateMachine:
             random_record = random.choice(filtered_records)
             return random_record["pixel_coords"], random_record["label"], random_record["angle"]
         return None, None, None
-
-    def calculate_center_point(self, contour):
-        target_pixel = [0, 0] 
-        M = cv2.moments(contour)
-        target_pixel[0] = float(M['m10'] / M['m00'])
-        target_pixel[1] = float(M['m01'] / M['m00'])
-        return target_pixel
